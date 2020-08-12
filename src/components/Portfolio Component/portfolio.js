4aeaceb498ceb43c89099ce62ca72ce5d51e91a2
Zachary Deacon <zackdeacon347@gmail.com>
2020-08-12 11:30:13 -0700

    converted portfolio page to use project component

--- src/components/Portfolio Component/portfolio.js
+++ src/components/Portfolio Component/portfolio.js
@@ -1,83 +1,62 @@
 import React from 'react'
 import "./style.css"
+import Project from "../Project Component/project"
+
 
 export default function Portfolio() {
     return (
         <div className="container">
         <div className="row row-cols-1 row-cols-md-3">
-            <div className="col-sm-5">
-              <div className="card card-p">
-                <h5 className="card-header">MixMe app!</h5>
-                  <a href="https://vinavarghese.github.io/ProjectMixMe/" target="_blank">
-                    <img src={require("../../Assets/MixMe2.jpeg")} className="card-img-top project-card-pic picSize" alt="Project link"></img>
-                    </a>
-                    <div className="card-footer card-footer-height">
-                      <p>MixMe is an interactive platform that allows users to browse their favorite cocktails, search cocktail recipes by ingredient, play an interactive game to learn about their favorite cocktails, and find bars in their local area that serve the cocktails. MixMe uses Javascript, Foundation (CSS), CSS shake, and two APIs to source data. Feel free to click the picture to check it out and let me know any feedback you may have!</p>
-                      <a href="https://github.com/VinAVarghese/ProjectMixMe" target="_blank" className="Github-link">Check out the Github Repo here!</a>
-                    </div>
-              </div>
-            </div>
-            <div className="col-sm-5">
-              <div className="card card-p">
-                <h5 className="card-header">PureCloud Electronics!</h5>
-                <a href="https://purecloudone.herokuapp.com/" target="_blank">
-                <img src={require("../../Assets/PureCloud.jpeg")} className="card-img-top project-card-pic picSize" alt="PureCloud app link"></img>
-                </a>
-                <div className="card-footer card-footer-height">
-                  <p>PureCloud Electronics is a digital storefront that uses MySQL (Sequelize), Handlebars, Stripe integration and more! It was built in the MVC format, and allows for user login/authentification. Feel free to click the picture to check it out and let me know any feedback you may have!</p>
-                  <a href="https://github.com/zackdeacon/Prime_Suspects" target="_blank" className="Github-link">Check out the Github Repo here!</a>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-5">
-              <div className="card card-p">
-                <h5 className="card-header">Quiz app!</h5>
-                <a href="https://zackdeacon.github.io/Password_generator_homework/" target="_blank">
-                <img src={require("../../Assets/Quiz2.jpeg")} className="card-img-top project-card-pic picSize" alt="Quiz app"></img>
-                </a>               
-                <div className="card-footer card-footer-height">
-                  <p>This is an quiz game that I developed! It will prompt you through a series of 5 questions and it has a timer that will end the game when time runs out. Once you have completed all of the questions, the remaining time will become your score. Feel free to click the picture to check it out and let me know any feedback you may have. Have fun!</p>
-                  <a href="https://github.com/zackdeacon/Quiz-homework" target="_blank" className="Github-link">Check out the Github Repo here!</a>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-5">
-              <div className="card card-p">
-                <h5 className="card-header">Weather Dashboard App!</h5>
-                <a href="https://zackdeacon.github.io/Weather-app-homework/" target="_blank">
-                <img src={require("../../Assets/WeatherApp2.jpeg")} className="card-img-top project-card-pic picSize" alt="weather app link"></img>
-                </a>
-                <div className="card-footer card-footer-height">
-                  <p>This app uses local storage, Javascript, CSS, and 3rd party API data to allow a user to search for the weather related info of a certain city. The app will provide current conditions, a five day forecast, and the UV index. 
+            <Project title="PureCloud Electronics"
+            about="PureCloud Electronics is a digital storefront that uses MySQL (Sequelize), Handlebars, Stripe integration and more! It was built in the MVC format, and allows for user login/authentification. Feel free to click the picture to check it out and let me know any feedback you may have!"
+            githubLink="Check out the Github Repo here!"
+            github="https://github.com/zackdeacon/Prime_Suspects"
+            alt="PureCloud Image"
+            pic={require("../../Assets/PureCloud.jpeg")}
+            projectLink="https://purecloudone.herokuapp.com/"
+            />
+            <Project title="MixMe app!"
+            about="MixMe is an interactive platform that allows users to browse their favorite cocktails, search cocktail recipes by ingredient, play an interactive game to learn about their favorite cocktails, and find bars in their local area that serve the cocktails. MixMe uses Javascript, Foundation (CSS), CSS shake, and two APIs to source data. Feel free to click the picture to check it out and let me know any feedback you may have!"
+            githubLink="Check out the Github Repo here!"
+            github="https://github.com/VinAVarghese/ProjectMixMe"
+            alt="Mixme Image"
+            pic={require("../../Assets/MixMe2.jpeg")}
+            projectLink="https://vinavarghese.github.io/ProjectMixMe/"
+            />
+            <Project title="React employee directory!"
+            about="This app is built using React and utilizes an API through Ajax. This app allows a user to search through the user directory and filter by first name and last name. Enjoy!"
+            githubLink="Check out the Github Repo here!"
+            github="https://github.com/zackdeacon/react-employee-directory-hw"
+            alt="React Directory Image"
+            pic={require("../../Assets/react-employee-directory.jpeg")}
+            projectLink="https://mighty-savannah-44325.herokuapp.com/"
+            />
+            <Project title="Quiz app!"
+            about="This is an quiz game that I developed! It will prompt you through a series of 5 questions and it has a timer that will end the game when time runs out. Once you have completed all of the questions, the remaining time will become your score. Feel free to click the picture to check it out and let me know any feedback you may have. Have fun!"
+            githubLink="Check out the Github Repo here!"
+            github="https://github.com/zackdeacon/Quiz-homework"
+            alt="Quiz app Image"
+            pic={require("../../Assets/Quiz2.jpeg")}
+            projectLink="https://zackdeacon.github.io/Quiz-homework"
+            />
+            <Project title="Weather Dashboard App!"
+            about="This app uses local storage, Javascript, CSS, and 3rd party API data to allow a user to search for the weather related info of a certain city. The app will provide current conditions, a five day forecast, and the UV index. 
     
-                    This app calls on data from a weather API. Feel free to click the picture to check it out and let me know any feedback you may have!</p>
-                    <a href="https://zackdeacon.github.io/Weather-app-homework/" target="_blank" className="Github-link">Check out the Github Repo here!</a>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-5">
-              <div className="card card-p">
-                <h5 className="card-header">React employee directory!</h5>
-                <a href="https://mighty-savannah-44325.herokuapp.com/" target="_blank">
-                <img src={require("../../Assets/react-employee-directory.jpeg")} className="card-img-top project-card-pic picSize" alt="employee directory app link"></img>
-                </a>
-                <div className="card-footer card-footer-height">
-                  <p>This app is built using React and utilizes an API through Ajax. This app allows a user to search through the user directory and filter by first name and last name. Enjoy!</p>
-                    <a href="https://github.com/zackdeacon/react-employee-directory-hw" target="_blank" className="Github-link">Check out the Github Repo here!</a>
-                </div>
-              </div>
-            {/* <div className="col mb-4">
-                <div className="card card-p">
-                    <h5 className="card-header">Project Title!</h5>
-                    <a href="https://www.disney.com/" target="_blank">
-                  <img src="https://placekitten.com/200/287" className="card-img-top" alt="...">
-                    </a>
-                    <div className="card-footer">
-                      <p>Text describing the project and its components</p>
-                    </div>
-                </div>
-            </div>     --> */}
-          </div>
+            This app calls on data from a weather API. Feel free to click the picture to check it out and let me know any feedback you may have!"
+            githubLink="Check out the Github Repo here!"
+            github="https://github.com/zackdeacon/Weather-app-homework"
+            alt="Weather App Image"
+            pic={require("../../Assets/WeatherApp2.jpeg")}
+            projectLink="https://zackdeacon.github.io/Weather-app-homework/"
+            />
+            <Project title="Restaraunt Burger App"
+            about="This is an app that will allow to keep track of different eaten burgers at a restaraunt. This app uses MySQL and allows users full CRUD abilities to Create, Remove, Update and Delete data from the database. This app is fully deployed to Heroku."
+            githubLink="Check out the Github Repo here!"
+            github="https://github.com/zackdeacon/burger_hw"
+            alt="Burger App Image"
+            pic={require("../../Assets/burger_hw_pic.jpeg")}
+            projectLink="https://zacks-burger-app.herokuapp.com/"
+            />
       </div> 
       </div>
         )
--- src/components/Project Component/project.js
+++ src/components/Project Component/project.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import "./style.css"
+import { Link } from 'react-router-dom'
 
-export default function project() {
+export default function project(props) {
     return (
         <div className="col-sm-5">
         <div className="card card-p">
-          <h5 className="card-header">PureCloud Electronics!</h5>
-          <a href="https://purecloudone.herokuapp.com/" target="_blank">
-          <img src={require("../../Assets/PureCloud.jpeg")} className="card-img-top project-card-pic picSize" alt="PureCloud app link"></img>
+          <h5 className="card-header">{props.title}</h5>
+          <a href={props.projectLink} target="_blank">
+          <img src={props.pic} className="card-img-top project-card-pic picSize" alt={props.alt}></img>
           </a>
           <div className="card-footer card-footer-height">
-            <p>PureCloud Electronics is a digital storefront that uses MySQL (Sequelize), Handlebars, Stripe integration and more! It was built in the MVC format, and allows for user login/authentification. Feel free to click the picture to check it out and let me know any feedback you may have!</p>
-            <a href="https://github.com/zackdeacon/Prime_Suspects" target="_blank" className="Github-link">Check out the Github Repo here!</a>
+            <p>{props.about}</p>
+            <a href={props.github} target="_blank" className="Github-link">{props.githubLink}</a>
           </div>
         </div>
       </div>
