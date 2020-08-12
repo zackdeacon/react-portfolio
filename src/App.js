ecea3944757642ba58f3d773eb87e2787e44c5c5
Zachary Deacon <zackdeacon347@gmail.com>
2020-08-11 19:12:41 -0700

    added router function

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Nav from './components/Nav Bar/Nav';
 import Footer from './components/Footer/footer';
 import Portfolio from './components/Portfolio Component/portfolio';
 import Contact from './components/Contact Component/contact';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 
 function App() {
   return (
+    <Router>
     <>
       <Nav />
-      <Home />
-      {/* <Portfolio /> */}
-      {/* <Contact /> */}
+      <Route exact path="/" component={Home} />
+      <Route exact path="/portfolio" component={Portfolio} />
+      <Route exact path="/contact" component={Contact} />
       <Footer />
       </>
+    </Router>
+      
   );
 }
 
--- src/components/Nav Bar/Nav.js
+++ src/components/Nav Bar/Nav.js
@@ -5,20 +5,20 @@ import "./style.css";
 export default function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="index.html">Zachary Deacon</a>
+        <a className="navbar-brand" href="/">Zachary Deacon</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="portfolio.html">Portfolio</a>
+              <a className="nav-link" href="/portfolio">Portfolio</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="contact.html">Contact</a>
+              <a className="nav-link" href="/contact">Contact</a>
             </li>
           </ul>
           <span className="navbar-text">
